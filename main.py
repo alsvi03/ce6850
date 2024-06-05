651d990f57363abefacfd0a4f288fb8baaf82840
alsvi03 <alexsvir14@mail.ru>
2024-06-05 16:00:07 +0300

    добавил расшифровку и другое

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ import crcmod
 import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)  # тут поменять на нужный
+r.delete(f'output')
 
 reqest: bytearray = [0] * 8
 confim: bytearray= [0]*6
 password: bytearray=[0]*14
 read_reqes: bytearray = [0]*15
+
+def calculate_crc(buff, count):
+    crc = 0
+    for i in range(count):
+        crc += buff[i]
+    return crc & 0x7F
 
 
 def int_to_hex(buff, size):
@@ -31,13 +38,17 @@ def int_to_hex(buff, size):
         out += buff2[i]
     return out
 
-def create_Packege_reqest():
+def create_Packege_reqest(address):
     global reqest
+
+    address_bytes = [ord(char) for char in str(address)]
+
+    for i in range(len(address_bytes)):
+        reqest[i + 2] = address_bytes[i]
+
     reqest[0] = 0x2f # /
     reqest[1] = 0x3f # ?
-    reqest[2] = 0x00 # адрес
-    reqest[3] = 0x00 # адрес
-    reqest[4] = 0x00 # адрес
+
     reqest[5] = 0x21 # !
     reqest[6] = 0x0d # возврат каретки
     reqest[7] = 0x0a # перевод строки
@@ -49,9 +60,9 @@ def create_confirm_msg():
     confim[1] = 0x30
     confim[2] = 0x35
     confim[3] = 0x31
-    reqest[4] = 0x0d  # возврат каретки
-    reqest[5] = 0x0a  # перевод строки
-    return reqest
+    confim[4] = 0x0d  # возврат каретки
+    confim[5] = 0x0a  # перевод строки
+    return confim
 
 def create_password_msg():
     global password
@@ -103,8 +114,32 @@ def create_Read_msg(com,I1,I2,e):
     read_reqes[11] = ord(str(I2))  # количество запрашиваемых месяцев (дней)
     read_reqes[12] = 0x29 # )
     read_reqes[13] = 0x03
-    read_reqes[14] = 0x0d # пхд контрольная сумма
+    #read_reqes[14] = 0x0d # пхд контрольная сумма
+    read_reqes[14] = calculate_crc(read_reqes, 13)  # пхд контрольная сумма
+
     return read_reqes
+
+
+def check_data(buffer):
+    values = []
+    in_range = False
+    current_string = ''
+    for value in buffer:
+        if value == '28':
+            in_range = True
+            current_string = ''
+        elif value == '29':
+            in_range = False
+            # Переводим строку из ASCII символов в числа
+            num_string = ''
+            for char in current_string:
+                num_string += char if char.isdigit() else '.'
+
+            values.append(num_string)
+        elif in_range:
+
+                current_string += chr(int(value, 16))  # Преобразуем ASCII код в символ и добавляем к текущей строке
+    return values
 
 
 
@@ -117,7 +152,7 @@ json_create_cmd = {
     "cmd": 'day',  # название типа опроса day - показания на начало суток
     "run": 'ce6850',  # название вызываемого протокола
     "vm_id": 4,  # id прибора учёта
-    "ph": 631,  # адрес под которым счетчик забит в успд
+    "ph": 573,  # адрес под которым счетчик забит в успд
     "trf": '3',  # количество тарифов у счётчика
     "ki": 2,  # коэф тока
     "ku": 3,  # коэф трансформации
@@ -145,7 +180,7 @@ com = json.loads(channel_command)["cmd"]
 
 answer_key = str(uuid.uuid4())  # создание ключа
 json_output = {"key": answer_key, "vmid": 4, "command": "day", "do": "send",
-               "out": int_to_hex(create_Packege_reqest(),8),
+               "out": int_to_hex(create_Packege_reqest(address),8),
                "protocol": "1",
                "waitingbytes": 28}  # генерируем json с запросом и указываем ключ куда положить ответ
 
@@ -180,12 +215,16 @@ for i in range (4):
 
 
 
+# for i in range(7):
+#     print(r.lpop('output'))
+# print(int_to_hex(create_Packege_reqest(address),8))
+# print(int_to_hex(create_confirm_msg(),6))
+# print(int_to_hex(create_password_msg(),14))
+# print(int_to_hex(create_Read_msg(com,I1,I2,0),15))
 
-
-
-#print(create_Read_msg(com,I1,I2))
-# print(int_to_hex(create_Packege_reqest(),8))
-# print(int_to_hex(create_Read_msg(com,I1,I2,1),14))
+buff = ['45', '44', '30', '50', '45', '28', '30', '30', '30', '30', '30', '30', '30', '30', '30', '33', '34', '36', '31', '2E', '32', '31', '33', '33', '34', '29', '0D', '0A',
+        '45', '44', '30', '50', '45', '28', '30', '30', '30', '30', '30', '30', '30', '30', '30', '31', '30', '38', '30', '2E', '36', '34', '38', '38', '36', '29', '0D']
+print(check_data(buff))
 
 
 
